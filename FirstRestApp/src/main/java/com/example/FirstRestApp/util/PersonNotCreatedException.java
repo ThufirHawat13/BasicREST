b244aa749770ea6374475ad8959615b4a3e165c7
ThufirHawat13 <kurnyshov1998@gmail.com>
2022-10-26 02:55:30 +0300

    Initial commit

---
+++ FirstRestApp/src/main/java/com/example/FirstRestApp/util/PersonNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.example.FirstRestApp.util;
+
+public class PersonNotCreatedException extends RuntimeException{
+    public PersonNotCreatedException(String msg) {
+        super(msg);
+    }
+}
