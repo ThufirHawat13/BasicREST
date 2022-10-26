a16eff9c3998d77250abb3c770f543f83af3aa68
ThufirHawat13 <kurnyshov1998@gmail.com>
2022-10-26 14:46:32 +0300

    initial commit

--- src/main/java/com/example/BasicREST/repositories/PeopleRepository.java
+++ src/main/java/com/example/BasicREST/repositories/PeopleRepository.java
@@ -9,10 +9,4 @@ import java.util.List;
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
 
-    List<Person> findByName(String name);
-
-    List<Person> findByEmail(String email);
-
-    List<Person> findByNameStartingWith(String startingWith);
-
 }
